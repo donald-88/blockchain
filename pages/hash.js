5d190e873acec39f7719d2871ef6a64d449c5f60
McDonald Namba <nambamcdonald@gmail.com>
2022-04-01 12:51:20 +0200

    finally

--- pages/hash.js
+++ pages/hash.js
@@ -21,7 +21,7 @@ const Hash = (props) =>{
           <div
             className="relative flex items-center justify-center h-full transition-transform transform bg-green border-2 border-black group-hover:-translate-x-2 group-hover:-translate-y-2">
             <div className="px-8 pb-8 flex flex-col items-center transition-opacity group-hover:opacity-0 group-hover:absolute">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -4 24 24" width="96" height="96" preserveAspectRatio="xMinYMin" class="jam jam-computer"><path d='M2 13v1h3V2H2v9h1v2H2zM1 0h5a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1zm9 3h8a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm0 2v6h8V5h-8zm2 9h4a1 1 0 0 1 0 2h-4a1 1 0 0 1 0-2z'/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -4 24 24" width="96" height="96" preserveAspectRatio="xMinYMin" className="jam jam-computer"><path d='M2 13v1h3V2H2v9h1v2H2zM1 0h5a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1zm9 3h8a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm0 2v6h8V5h-8zm2 9h4a1 1 0 0 1 0 2h-4a1 1 0 0 1 0-2z'/></svg>
 
               <h2 className="mt-4 w-64 flex justify-center text-2xl font-medium">HASH</h2>
             </div>
